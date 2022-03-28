ba8a66a49ad3759dfd18d55c996c168670c163e2
Saul-t16 <saul_gonzalo_tenesaca_tsapp1oa2021@cev.com>
2022-03-28 19:37:44 +0200

    añadiendo datos de mascota

--- app/src/main/java/com/example/walkmypet/DataPetActivity.java
+++ app/src/main/java/com/example/walkmypet/DataPetActivity.java
@@ -1,6 +1,29 @@
 package com.example.walkmypet;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class DataPetActivity extends AppCompatActivity {
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView =findViewById(R.id.listview);
+        ArrayList<String> arrayList= new ArrayList<>();
+
+        arrayList.add("cuidador 1");
+        arrayList.add("cuidador 2");
+        arrayList.add("cuidador 3");
+        arrayList.add("cuidador 4");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+    }
 }
